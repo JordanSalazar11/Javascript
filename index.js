a6e03fa40713cdc363201f547bd6a3357cfb2312
JordanSalazar11 <jordanstevensalazarpadilla@gmail.com>
2024-06-18 11:01:13 -0500

    javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+function onClickSubmitButton(){
+    let inputValue = document.getElementById("inputText").Value;
+    if(inputValue>0){
+        alert("el numero es mayor a cero")
+    } if(inputValue<0){
+        alert("el numero es menor a cero")
+    } if(inputValue=0){
+        alert("el numero es igual a cero")
+    }
+    
+    
+}
